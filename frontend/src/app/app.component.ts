018c1d814f8fca578ed18649150733d26c463900
Felix Burk <felix.burk@googlemail.com>
2020-04-25 00:42:16 +0200

    navbar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('collapse', [
+      state('open', style({
+        opacity: '1'
+      })),
+      state('closed', style({
+        opacity: '0',
+        display: 'none',
+      })),
+      transition('closed => open', animate('400ms ease-in')),
+      transition('open => closed', animate('100ms ease-out'))
+    ])
+  ]
 })
 export class AppComponent {
   title = 'cooksmart';
+
+  isNavbarCollapsed = true;
+  _isNavbarCollapsedAnim = 'closed';
+
+  veil = document.createElement('div');
+
+  toggleNavbar(event: MouseEvent) {
+    if (this.isNavbarCollapsed) {
+      this._isNavbarCollapsedAnim = 'open';
+      this.isNavbarCollapsed = false;
+    } else {
+      this.closeNavbar();
+    }
+  }
+
+  closeNavbar() {
+    this._isNavbarCollapsedAnim = 'closed';
+    this.isNavbarCollapsed = true;
+  }
+
+  get isNavbarCollapsedAnim(): string {
+    return this._isNavbarCollapsedAnim;
+  }
 }
