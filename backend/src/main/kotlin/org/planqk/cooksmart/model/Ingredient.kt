397b45ff3f172214d4667ccc3c7b817b99877cf1
Tim Niederhausen <st144339@stud.uni-stuttgart.de>
2020-04-24 23:55:41 +0200

    Don't serialize prototype and recipe members in Ingredient
    They just act as foreign keys here and are not supposed to be exposed directly.

--- backend/src/main/kotlin/org/planqk/cooksmart/model/Ingredient.kt
+++ backend/src/main/kotlin/org/planqk/cooksmart/model/Ingredient.kt
@@ -24,7 +24,6 @@ data class Ingredient(
         val id: Long = 0,
 
         @ManyToOne(cascade = [CascadeType.REMOVE])
-        @JsonProperty
         val prototype: IngredientPrototype,
 
         @JsonProperty
@@ -35,6 +34,5 @@ data class Ingredient(
         val unit: String = "",
 
         @ManyToOne(cascade = [CascadeType.REMOVE])
-        @JsonProperty
         val recipe: Recipe
 )
