142115c8d86c84e1862f8008b119e7a3f8f12bad
Tim Niederhausen <st144339@stud.uni-stuttgart.de>
2020-04-25 03:14:12 +0200

    Remove unnecessary @ComponentScan annotation

--- backend/src/main/kotlin/org/planqk/cooksmart/CooksmartApplication.kt
+++ backend/src/main/kotlin/org/planqk/cooksmart/CooksmartApplication.kt
@@ -18,7 +18,6 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
-@ComponentScan(basePackages = ["org.planqk.cooksmart", "org.planqk.cooksmart.api", "org.planqk.cooksmart.api.model"])
 class CooksmartApplication
 
 fun main(args: Array<String>) {
