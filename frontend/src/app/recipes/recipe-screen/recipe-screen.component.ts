5f6ebca424851379b853275a94756d3eef94fc35
Tim Niederhausen <st144339@stud.uni-stuttgart.de>
2020-04-30 20:42:59 +0200

    Implement recipe searching

--- frontend/src/app/recipes/recipe-screen/recipe-screen.component.ts
+++ frontend/src/app/recipes/recipe-screen/recipe-screen.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe, RecipeService } from '../../data';
+import { Observable, of, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-recipe-screen',
   templateUrl: './recipe-screen.component.html',
-  styleUrls: ['./recipe-screen.component.scss']
+  styleUrls: ['./recipe-screen.component.scss'],
 })
 export class RecipeScreenComponent implements OnInit {
+  recipes$: Observable<Recipe[]> = of([]);
+  private searchTerms = new Subject<string>();
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) {}
 
   ngOnInit() {
+    this.recipes$ = this.searchTerms.pipe(
+      // wait 300ms after each keystroke before considering the term
+      debounceTime(300),
+
+      // ignore new term if same as previous term
+      distinctUntilChanged(),
+
+      // switch to new search observable each time the term changes
+      switchMap((term: string) => this.recipeService.listRecipes(term)),
+    );
   }
 
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
 }
