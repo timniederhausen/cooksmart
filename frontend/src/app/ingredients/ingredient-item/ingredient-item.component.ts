88fe5399dd35eb60f534eea5f8fee7c0fcde863c
Felix Burk <felix.burk@googlemail.com>
2020-05-02 23:02:13 +0200

    first steps for ingredient list + copyright headers

--- frontend/src/app/ingredients/ingredient-item/ingredient-item.component.ts
+++ frontend/src/app/ingredients/ingredient-item/ingredient-item.component.ts
@@ -1,4 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+// Copyright 2020 Felix Burk, Tim Niederhausen
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+import { Component, Input, OnInit } from '@angular/core';
+import { Ingredient } from '../../data';
 
 @Component({
   selector: 'app-ingredient-item',
@@ -6,10 +20,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ingredient-item.component.scss']
 })
 export class IngredientItemComponent implements OnInit {
+  @Input()
+  ingredient: Ingredient = undefined;
 
-  constructor() { }
+  editing: boolean = false;
 
-  ngOnInit(): void {
+  ngOnInit() {
+  }
+
+  toggleEdit() {
+    this.editing = !this.editing;
+  }
+
+  saveEdit() {
+    //TODO
+  }
+
+  changeRate() {
+    //TODO
   }
 
 }
--- frontend/src/app/ingredients/ingredient-screen/ingredient-screen.component.ts
+++ frontend/src/app/ingredients/ingredient-screen/ingredient-screen.component.ts
@@ -1,4 +1,20 @@
+// Copyright 2020 Felix Burk, Tim Niederhausen
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 import { Component, OnInit } from '@angular/core';
+import { Observable, of, Subject } from 'rxjs';
+import { Ingredient, IngredientService, Recipe } from '../../data';
+import { SmartListComponent } from '../../smart-list/smart-list.component';
 
 @Component({
   selector: 'app-ingredient-screen',
@@ -6,10 +22,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ingredient-screen.component.scss']
 })
 export class IngredientScreenComponent implements OnInit {
+  ingredients$: Observable<Ingredient[]> = of(
+    [{id:0,prototype:{image:"",id:0,description:"looooong placeholder", name:"pizza"},
+      quantity:10,unit:"kg"}]);
+  private searchTerms = new Subject<string>();
 
-  constructor() { }
+  ingredientList: SmartListComponent<Recipe>;
 
-  ngOnInit(): void {
+  constructor(private ingredientService: IngredientService) {}
+
+  ngOnInit() {
   }
 
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
 }
