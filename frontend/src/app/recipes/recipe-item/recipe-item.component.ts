064eb0a4336f63987698db07d140b9a4aad9a3ad
Felix Burk <felix.burk@googlemail.com>
2020-05-02 20:19:24 +0200

    license header for frontend

--- frontend/src/app/recipes/recipe-item/recipe-item.component.ts
+++ frontend/src/app/recipes/recipe-item/recipe-item.component.ts
@@ -1,5 +1,18 @@
+// Copyright 2020 Felix Burk, Tim Niederhausen
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 import { Component, Input, OnInit } from '@angular/core';
-import { Recipe } from '../../data';
+import { Ingredient, Recipe } from '../../data';
 
 @Component({
   selector: 'app-recipe-item',
@@ -8,7 +21,7 @@ import { Recipe } from '../../data';
 })
 export class RecipeItemComponent implements OnInit {
   @Input()
-  recipe: Recipe = { description: 'placeholder', id: 0, image: '', ingredients: undefined, rating: 2, name:"placeholder"};
+  recipe: Recipe = undefined;
 
   ngOnInit() {
   }
