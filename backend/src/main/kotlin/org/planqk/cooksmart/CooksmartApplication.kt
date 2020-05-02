886b668a2357578687ad7e36f01e2cfa59e64074
Tim Niederhausen <st144339@stud.uni-stuttgart.de>
2020-05-03 00:25:21 +0200

    Enable CORS for the entire API

--- backend/src/main/kotlin/org/planqk/cooksmart/CooksmartApplication.kt
+++ backend/src/main/kotlin/org/planqk/cooksmart/CooksmartApplication.kt
@@ -15,10 +15,21 @@ package org.planqk.cooksmart
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
-class CooksmartApplication
+class CooksmartApplication {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+            }
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<CooksmartApplication>(*args)
