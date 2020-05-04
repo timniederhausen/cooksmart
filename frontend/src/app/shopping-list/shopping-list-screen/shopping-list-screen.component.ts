4339e98c6653666439235d3724760f9ba3377b79
Felix Burk <felix.burk@googlemail.com>
2020-05-05 01:01:45 +0200

    shopping list backend stuff

--- frontend/src/app/shopping-list/shopping-list-screen/shopping-list-screen.component.ts
+++ frontend/src/app/shopping-list/shopping-list-screen/shopping-list-screen.component.ts
@@ -11,9 +11,13 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-import { Component, OnInit } from '@angular/core';
-import { Recipe } from '../../data';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IngredientPrototype, Recipe, RecipeService, ShoppingListService } from '../../data';
 import { Ingredient } from '../../data';
+import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map, switchMap } from 'rxjs/operators';
+import { NgbTypeahead, NgbTypeaheadSelectItemEvent } from '@ng-bootstrap/ng-bootstrap';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-list-screen',
@@ -24,21 +28,64 @@ export class ShoppingListScreenComponent implements OnInit {
   recipeList: Recipe[] = [];
   ingredientList: Ingredient[] = [];
 
+  refresh$ = new BehaviorSubject<void>(undefined);
+
+  @ViewChild('instance', { static: true }) instance: NgbTypeahead;
+
+  constructor(private readonly shoppingListService: ShoppingListService,
+              private readonly recipeService: RecipeService) {
+  }
+
   ngOnInit(): void {
     for (let recipe of this.recipeList) {
       this.addRecipe(recipe);
     }
   }
 
+  refreshList() {
+    this.shoppingListService.compileForRecipes(this.recipeList.map(a => a.id)).subscribe(
+      (res) => {
+        this.ingredientList = res;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
   addRecipe(recipe: Recipe) {
-    for (let ingredient of recipe.ingredients) {
-      this.ingredientList.push(ingredient);
-    }
+    this.recipeList.push(recipe);
+    this.refreshList();
   }
 
   removeRecipe(id: number) {
     this.recipeList = this.recipeList.filter((ing) => ing.id !== id);
+    this.refreshList();
   }
 
-  search(value: string) {}
+  recipeSearchFormatter = (result: Recipe) => result.name;
+  test = "";
+
+  recipeSearch = (text$: Observable<string>) => {
+    return combineLatest([text$, this.refresh$]).pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      switchMap(([term]) =>
+        this.recipeService
+          .listRecipes(term).pipe(map(p => p.content)),
+      ),
+    );
+  };
+
+  selectedRecipe($event: NgbTypeaheadSelectItemEvent) {
+    this.addRecipe($event.item);
+  }
+
+  public onFocus(e: Event): void {
+    e.stopPropagation();
+    setTimeout(() => {
+      const inputEvent: Event = new Event('input');
+      e.target.dispatchEvent(inputEvent);
+    }, 0);
+  }
 }
